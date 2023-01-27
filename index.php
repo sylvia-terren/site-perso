257789a46a91b8f8f38393146d619f351bd8cd13
Sylvia <sylvia.terren@laplateforme.io>
2023-01-27 10:08:40 +0100

    Push du jour d'avant (création des fichiers et de la maquette via figma, dossier google drive)

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="This is my first portfolio test!"> <!-- 160 caractères -->
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <script type="text/javascript" src="scripts.js"></script>
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <title>Sylvia Terren | Portfolio</title>
+</head>
+<body>
+    
+</body>
+</html>
